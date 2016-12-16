f63962b07a574f158fbc1fddb9acea7b2b6bab42
egbertn <egbert_nierop@outlook.com>
2016-12-16 16:51:55 +0100

    nuget release

--- src/Demo/CookieWebCore/Controllers/HomeController.cs
+++ src/Demo/CookieWebCore/Controllers/HomeController.cs
@@ -5,8 +5,9 @@ namespace CookieWebCore.Controllers
 {
     public class HomeController : BaseController
     {
-        public IActionResult Default()
+        public IActionResult Default([FromQuery] string page)
         {
+          
             var model = new HomeModel();
             model.CountRefresh =  (int)(Session["CountRefresh"] ?? 0);
             model.CountRefresh++;
@@ -20,10 +21,65 @@ namespace CookieWebCore.Controllers
             }
             return View(model);
         }
-    
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            Session.Remove("Loggedin");
+            return RedirectToAction("Default");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            /*
+             * Dim RF
+Set RF = Request.Form
+'Stop
+If RF("ACTION") = "OK" Then
+	If RF("UserID") = "demo" And RF("Password") = "demo" Then
+		Session("LoggedIn") = True
+
+		Response.Redirect "?page=securepage"
+	End If
+	Session("ErrMsg") = "Please try again, your user id / password  was wrong"
+
+End If
+             * */
+            var login = new LoginModel();
+
+            return View(login);
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel login)
+        {
+            /*
+             * Dim RF
+Set RF = Request.Form
+'Stop
+If RF("ACTION") = "OK" Then
+	If RF("UserID") = "demo" And RF("Password") = "demo" Then
+		Session("LoggedIn") = True
+
+		Response.Redirect "?page=securepage"
+	End If
+	Session("ErrMsg") = "Please try again, your user id / password  was wrong"
+
+End If
+             * */
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return PartialView(login);
+        }
         public IActionResult Ajax()
         {
-            return View();
+            var model = new HomeModel();
+            model.CountRefresh = (int)(Session["CountRefresh"] ?? 0);
+            model.CountRefresh++;
+            Session["CountRefresh"] = model.CountRefresh;
+            // again, this is not how it should be done, but to keep the code samples a little bit the same            
+            model.Session = this.Session;
+            return PartialView(model);
         }
         public IActionResult Error()
         {
--- src/Demo/CookieWebCore/wwwroot/js/site.js
+++ src/Demo/CookieWebCore/wwwroot/js/site.js
@@ -5,20 +5,23 @@ $( document ).ready(function()
 {
         
     function doAjaxPanel(refresh, path) {
-        //$.get(  path  + "&ajaxRefresh=" + (refresh ? "1" : "0"),
-        //       function( data ) { $( "div.default_content" ).html( data );}
-
-        //        ); 
+        
+        $.get(  path  + "&ajaxRefresh=" + (refresh ? "1" : "0"),
+               function( data ) { $( "div.default_content" ).html( data );}
+        ); 
     };
 
     $("button.refresh").click(function(){ window.location.reload();});
     // how to pass the session to an Ajax panel.   
     $("button.ajax").click(function (e) {
         var me = $(this);
+        console.log('button.ajax');
         doAjaxPanel(true, me.attr("data-path")); return false;
     });
             
     if ($(".main").length > 0) //Default_Content.asp tag
+    {        
         doAjaxPanel(false, $("button.ajax").attr("data-path"));//default call
+    }
 });
         

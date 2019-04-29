5a0021345302496cf1c51aeac2f6c8211c5a0188
egbertn <egbert_nierop@outlook.com>
2019-04-29 21:38:33 +0200

    1.0.22

--- src/ispsession.io.core/Properties/AssemblyInfo.cs
+++ src/ispsession.io.core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("ADC Cure")]
 [assembly: AssemblyProduct("ISPSession Asp.Net Core")]
 [assembly: AssemblyVersion("9.0.161")]
-[assembly: AssemblyFileVersion("9.0.0.162")]
+[assembly: AssemblyFileVersion("9.0.0.163")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyTitle("ASP.NET Session Broker")]
 [assembly: AssemblyDescription("ASP.NET Session Broker Replacement Middleware And Global Cache MiddleWare for AspNetCore.\r\nSession State brings you back to what was possible in former ASP.NET days.\r\nGlobal Cache brings you amazing caching of expensive data, using Redis Server power.")]
